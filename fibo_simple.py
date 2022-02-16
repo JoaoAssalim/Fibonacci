71e986d828a4246ebaccef82a3aa1d1cce2175b7
João Assalim <62481051+JoaoAssalim@users.noreply.github.com>
2022-02-15 23:29:32 -0300

    Update fibo_simple.py

--- fibo_simple.py
+++ fibo_simple.py
@@ -2,6 +2,5 @@ length = input('Enter a length to fibonacci: ')
 if length.isdigit():
     prox, anterior = 0, 1
     for i in range(int(length)):
-        soma = prox + anterior
-        prox, anterior = soma, prox
+        prox, anterior = prox+anterior, prox
         print(soma)
